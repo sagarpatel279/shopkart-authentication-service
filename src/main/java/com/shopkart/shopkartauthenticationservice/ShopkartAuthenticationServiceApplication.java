b34974d498679f63b006f7b457241ff156c60f86
sagarpatel279 <patelsagar913@gmail.com>
2025-01-03 13:56:50 +0530

    initialized the project

---
+++ src/main/java/com/shopkart/shopkartauthenticationservice/ShopkartAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shopkart.shopkartauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopkartAuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopkartAuthenticationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shopkart/shopkartauthenticationservice/ShopkartAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopkart.shopkartauthenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkartAuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
